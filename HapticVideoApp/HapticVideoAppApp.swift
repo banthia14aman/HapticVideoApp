a29c9d2cea95e5d3d7425def07f9d810602f85b0
Aman Banthia <amanbanthia@gmail.com>
2025-12-12 00:19:55 +0530

    Initial Commit

---
+++ HapticVideoApp/HapticVideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticVideoAppApp.swift
+//  HapticVideoApp
+//
+//  Created by Aman Banthia on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HapticVideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HapticVideoAppTests/HapticVideoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HapticVideoAppTests.swift
+//  HapticVideoAppTests
+//
+//  Created by Aman Banthia on 12/12/25.
+//
+
+import Testing
+@testable import HapticVideoApp
+
+struct HapticVideoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
